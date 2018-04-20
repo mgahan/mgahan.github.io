7351cf1846836c40688a1f7b90a0da8c2d910982
mgahan <mgahan@metabiota.com>
2018-04-20 16:25:39 -0700

    test

--- Build.R
+++ Build.R
@@ -1,3 +1,4 @@
 library(blogdown)
 blogdown::new_site(theme = "xzya/hugo-bootstrap")
 blogdown::hugo_build()
+blogdown::new_post("Docker AWS")
---
+++ content/post/2018-04-20-docker-aws.md
@@ -0,0 +1,9 @@
+---
+title: Docker AWS
+author: ~
+date: '2018-04-20'
+slug: docker-aws
+categories: []
+tags: []
+---
+
